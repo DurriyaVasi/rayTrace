80d010eea4127c271d71e3090b8f1056c1292ca8
dvasi <dvasi@gl34.student.cs.uwaterloo.ca>
2018-03-01 15:06:26 -0500

    added scene

---
+++ Assets/sample.lua
@@ -0,0 +1,27 @@
+
+mat1 = gr.material({0.7, 1.0, 0.7}, {0.5, 0.7, 0.5}, 25)
+mat2 = gr.material({0.5, 0.5, 0.5}, {0.5, 0.7, 0.5}, 25)
+mat3 = gr.material({1.0, 0.6, 0.1}, {0.5, 0.7, 0.5}, 25)
+mat4 = gr.material({0.7, 0.6, 1.0}, {0.5, 0.4, 0.8}, 25)
+
+scene_root = gr.node('root')
+
+s2 = gr.nh_sphere('s2', {200, 50, -100}, 150)
+scene_root:add_child(s2)
+s2:set_material(mat1)
+
+b1 = gr.nh_box('b1', {-200, -125, 0}, 100)
+scene_root:add_child(b1)
+b1:set_material(mat4)
+
+steldodec = gr.mesh( 'dodec', 'smstdodeca.obj' )
+steldodec:set_material(mat3)
+scene_root:add_child(steldodec)
+
+white_light = gr.light({-100.0, 150.0, 400.0}, {0.9, 0.9, 0.9}, {1, 0, 0})
+orange_light = gr.light({400.0, 100.0, 150.0}, {0.7, 0.0, 0.7}, {1, 0, 0})
+
+gr.render(scene_root, 'nonhier.png', 256, 256,
+          {0, 0, 800}, {0, 0, -1}, {0, 1, 0}, 50,
+          {0.3, 0.3, 0.3}, {white_light, orange_light})
+
